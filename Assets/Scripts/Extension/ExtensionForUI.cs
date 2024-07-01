7cd8e048aedb6f2ef523abec1fb2ee51b3d34a58
bbbasya <42473270+asyaB404@users.noreply.github.com>
2024-07-01 22:12:59 +0800

    no message

--- Assets/Scripts/Extension/ExtensionForUI.cs
+++ Assets/Scripts/Extension/ExtensionForUI.cs
@@ -5,11 +5,18 @@ namespace Extension
 {
     public static class ExtensionForUI
     {
-        // 定义一个扩展方法来获取行数
-        public static void SetTextGUIHeight(this TextMeshProUGUI textMeshProUGUI)
+        public static void SetHeightFromText(this TextMeshProUGUI textMeshProUGUI)
         {
             int line = textMeshProUGUI.GetTextInfo(textMeshProUGUI.text).lineCount;
             float height = textMeshProUGUI.rectTransform.rect.height;
+            Vector2 sizeDelta = textMeshProUGUI.rectTransform.sizeDelta;
+            sizeDelta.y = line * height;
+            textMeshProUGUI.rectTransform.sizeDelta = sizeDelta;
+        }
+
+        public static void SetHeightFromText(this TextMeshProUGUI textMeshProUGUI, float height)
+        {
+            int line = textMeshProUGUI.GetTextInfo(textMeshProUGUI.text).lineCount;
             Vector2 sizeDelta = textMeshProUGUI.rectTransform.sizeDelta;
             sizeDelta.y = line * height;
             textMeshProUGUI.rectTransform.sizeDelta = sizeDelta;
