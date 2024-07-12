0d7a2ecccef352d09268706565fd789e807f1de8
bbbasya <42473270+asyaB404@users.noreply.github.com>
2024-07-12 20:50:04 +0800

    no message

--- Assets/Scripts/Framework/ResourcesMgr.cs
+++ Assets/Scripts/Framework/ResourcesMgr.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ResourcesMgr
 {
@@ -15,7 +17,25 @@ public class ResourcesMgr
         }
 
         _resDict[path] = Resources.Load<T>(path);
-        return _resDict[path] as T;
+        return (T)_resDict[path];
+    }
+    
+    //异步加载资源
+    public void LoadAsync<T>(string name, UnityAction<T> callback) where T:Object
+    {
+        ScenesMgr.Instance.StartCoroutine(ReallyLoadAsync(name, callback));
+    }
+
+    //真正的协同程序函数  用于 开启异步加载对应的资源
+    private IEnumerator ReallyLoadAsync<T>(string name, UnityAction<T> callback) where T : Object
+    {
+        ResourceRequest r = Resources.LoadAsync<T>(name);
+        yield return r;
+
+        if (r.asset is GameObject)
+            callback(Object.Instantiate(r.asset) as T);
+        else
+            callback(r.asset as T);
     }
 
     public void Clear()
