2806e674b915cfdcafd28db2d867258c6f38907b
bbbasya <42473270+asyaB404@users.noreply.github.com>
2024-07-13 21:59:33 +0800

    Revert "Revert "Revert "Revert "我搞了啥""""
    This reverts commit eb1932fbf16baee17f09b2cb0ac4333c577878b0.

--- Assets/Scripts/UI/StartPanel.cs
+++ Assets/Scripts/UI/StartPanel.cs
@@ -1,25 +1,25 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using DG.Tweening;
-using UnityEngine;
+using UnityEngine.UI;
+
 
 public class StartPanel : BasePanel<StartPanel>
 {
-    private void Awake()
+    public override void Init()
     {
-
+        base.Init();
+        GetControl<Button>("Button2")?.onClick.AddListener(() => { });
     }
 
     public override void CallBack(bool flag)
     {
         if (flag)
         {
+            MyCanvasGroup.interactable = true;
             gameObject.SetActive(true);
         }
         else
         {
-            
+            MyCanvasGroup.interactable = false;
+            gameObject.SetActive(false);
         }
     }
 }
