4b7d1599df9e06b4ba2414e0d5db7077ca54e404
bbbasya <42473270+asyaB404@users.noreply.github.com>
2024-07-15 02:52:26 +0800

    已经有头绪了

--- Assets/Scripts/UI/CreateRoomPanel.cs
+++ Assets/Scripts/UI/CreateRoomPanel.cs
@@ -1,12 +1,20 @@
+using FishNet.Transporting;
+using FishNet.Transporting.Tugboat;
 using TMPro;
 using UnityEngine;
 
 public class CreateRoomPanel : BasePanel<CreateRoomPanel>
 {
+    [SerializeField] private Tugboat tugboat;
+
     public override void Init()
     {
         base.Init();
-        GetControl<TMP_InputField>("IP").onEndEdit.AddListener((s) => { Debug.Log(s); });
-        GetControl<TMP_InputField>("port").onEndEdit.AddListener((s) => { Debug.Log(s); });
+        tugboat = FindObjectOfType<Tugboat>();
+        GetControl<TMP_InputField>("IP").onValueChanged.AddListener((s) =>
+        {
+            tugboat.SetServerBindAddress(s, IPAddressType.IPv4);
+        });
+        GetControl<TMP_InputField>("port").onValueChanged.AddListener((s) => { tugboat.SetPort(ushort.Parse(s)); });
     }
 }
--- Assets/Scripts/UI/JoinRoomPanel.cs
+++ Assets/Scripts/UI/JoinRoomPanel.cs
@@ -1,12 +1,17 @@
+using FishNet.Transporting;
+using FishNet.Transporting.Tugboat;
 using TMPro;
 using UnityEngine;
 
 public class JoinRoomPanel : BasePanel<JoinRoomPanel>
 {
+    [SerializeField] private Tugboat tugboat;
+
     public override void Init()
     {
         base.Init();
-        GetControl<TMP_InputField>("IP").onEndEdit.AddListener((s) => { Debug.Log(s); });
-        GetControl<TMP_InputField>("port").onEndEdit.AddListener((s) => { Debug.Log(s); });
+        tugboat = FindObjectOfType<Tugboat>();
+        GetControl<TMP_InputField>("IP").onValueChanged.AddListener((s) => { tugboat.SetClientAddress(s); });
+        GetControl<TMP_InputField>("port").onValueChanged.AddListener((s) => { tugboat.SetPort(ushort.Parse(s)); });
     }
 }
